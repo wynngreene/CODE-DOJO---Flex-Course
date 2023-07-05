377431b8c55d5482660309b33a9ae157b13af1a4
Wynn Greene <wynngreene.code@gmail.com>
2023-07-04 23:30:26 -0600

    Upload DOJO ALGO Challenges

---
+++ ALGO_Challenges/01_Get_1_to_255.js
@@ -0,0 +1,11 @@
+function get_array(startNumb,finishNumb) {
+    var arr = [];
+
+    for (var i = startNumb ; i = finishNumb; i++)
+    {
+        arr += arr.push[i];
+    }
+    return arr;
+};
+
+console.log(get_array(2,10));
