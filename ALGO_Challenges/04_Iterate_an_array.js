d91a4987353acf530c539a413665ac1103da3c3f
Wynn Greene <wynngreene.code@gmail.com>
2023-07-05 13:56:09 -0600

    Update 04_Iterate_an_array.js - add challenge directions for each function.

--- ALGO_Challenges/04_Iterate_an_array.js
+++ ALGO_Challenges/04_Iterate_an_array.js
@@ -1,3 +1,4 @@
+//Write a function that returns the sum of all the values within an array. (e.g. [1,2,5] returns 8, [-5,2,5,12] returns 14)
 function iterArr(arr) {
     //your code here 
     return sum; 
