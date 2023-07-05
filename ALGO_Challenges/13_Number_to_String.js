65e51c38ffe5b16874e830834ec9ee0bb1ee1601
Wynn Greene <wynngreene.code@gmail.com>
2023-07-05 14:01:24 -0600

    Update 13_Number_to_String.js - add challenge directions for each function.

--- ALGO_Challenges/13_Number_to_String.js
+++ ALGO_Challenges/13_Number_to_String.js
@@ -1,3 +1,6 @@
+// Write a function that takes an array of numbers and replaces any negative values within the array with the string 'Dojo'.
+// For example if array = [-1,-3,2], your function will return ['Dojo','Dojo',2].
+
 function numToStr(arr) {
     //your code here 
     return arr; 
