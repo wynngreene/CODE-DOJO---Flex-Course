2966e8b0caee10f6bf4aaab3cebd45a9158aaf4f
Wynn Greene <wynngreene.code@gmail.com>
2023-07-05 13:54:12 -0600

    Update 02_Get_even_1000.js - add challenge directions for each function.

--- ALGO_Challenges/02_Get_even_1000.js
+++ ALGO_Challenges/02_Get_even_1000.js
@@ -1,3 +1,4 @@
+//Write a function that would get the sum of all the even numbers from 1 to 1000. You may use a modulus operator for this exercise.
 function sum_even_numbers(){
     var sum = 0;
     //your code here 
