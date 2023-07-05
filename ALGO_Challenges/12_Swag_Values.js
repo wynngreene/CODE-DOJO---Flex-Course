1666777cd935f9e3a5b0f81f7a041a3e50575876
Wynn Greene <wynngreene.code@gmail.com>
2023-07-05 14:00:46 -0600

    Update 12_Swag_Values.js - add challenge directions for each function.

--- ALGO_Challenges/12_Swag_Values.js
+++ ALGO_Challenges/12_Swag_Values.js
@@ -1,3 +1,6 @@
+// Write a function that will swap the first and last values of any given array.
+// The default minimum length of the array is 2. (e.g. [1,5,10,-2] will become [-2,5,10,1]).
+
 function swap(arr) {
     //your code here 
     return arrnew; 
