e33a8499ab89b7a2d72662b6c5fb1be0f3a939d4
Wynn Greene <wynngreene.code@gmail.com>
2023-07-05 14:00:17 -0600

    Update 11_Max_MIn_Avg.js - add challenge directions for each function.

--- ALGO_Challenges/11_Max_MIn_Avg.js
+++ ALGO_Challenges/11_Max_MIn_Avg.js
@@ -1,3 +1,6 @@
+// Given an array with multiple values, write a function that returns a new array that only contains the maximum, minimum, and average values of the original array.
+// (e.g. [1,5,10,-2] will return [10,-2,3.5])
+
 function maxMinAvg(arr) {
     //your code here 
     return arrnew; 
