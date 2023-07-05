c18308691932e6c92712c9c7c63f84461a7470d4
Wynn Greene <wynngreene.code@gmail.com>
2023-07-05 13:57:44 -0600

    Update 07_Array_odd.js - add challenge directions for each function.

--- ALGO_Challenges/07_Array_odd.js
+++ ALGO_Challenges/07_Array_odd.js
@@ -1,3 +1,5 @@
+//Write a function that would return an array of all the odd numbers between 1 to 50.
+//(ex. [1,3,5, .... , 47,49]). Hint: Use 'push' method.
 function oddNumbers() {
     var arr = [];
     //your code here 
