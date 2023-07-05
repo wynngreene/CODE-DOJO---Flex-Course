37ac23344a1e525e6c1c202ec5a3d5d6466c6c84
Wynn Greene <wynngreene.code@gmail.com>
2023-07-05 13:58:49 -0600

    Update 09_Squares.js - Given an array with multiple values, write a function that replaces each value in the array with the product of the original value multiplied by itself. (e.g. [1,5,10,-2] will become [1,25,100,4])

--- ALGO_Challenges/09_Squares.js
+++ ALGO_Challenges/09_Squares.js
@@ -1,3 +1,6 @@
+//Given an array with multiple values, write a function that replaces each value in the array with the product of the original value multiplied by itself.
+//(e.g. [1,5,10,-2] will become [1,25,100,4])
+
 function squareVal(arr) {
     //your code here 
     return arr; 
