e16b39e45dea49d2eea2317df29b166430852cf3
Wynn Greene <wynngreene.code@gmail.com>
2023-07-05 13:57:13 -0600

    Update 06_Find_average.js - add challenge directions for each function.

--- ALGO_Challenges/06_Find_average.js
+++ ALGO_Challenges/06_Find_average.js
@@ -1,3 +1,5 @@
+//Given an array with multiple values,write a function that returns the average of the values in the array.
+//(e.g. for [1,3,5,7,20] average is 7.2)
 function findMax(arr) {
     //your code here 
     return max; 
