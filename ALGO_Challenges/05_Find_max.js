509526c09c7f60d2d58553b92fb72a82c1d8a859
Wynn Greene <wynngreene.code@gmail.com>
2023-07-13 14:44:05 -0600

    Update function

--- ALGO_Challenges/05_Find_max.js
+++ ALGO_Challenges/05_Find_max.js
@@ -1,5 +1,9 @@
 //Given an array with multiple values, write a function that returns the maximum number in the array. (e.g. for [-3,3,5,7] max is 7)
 function findMax(arr) {
-    //your code here 
+    max = 0;
+        //your code here 
+    for (i=0: i == arr.length; i++){
+        arr[i] + max;
+    }
     return max; 
 }
