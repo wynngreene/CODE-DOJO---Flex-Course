4445a9f32733795379313f0a65d8c9e11d04d733
Wynn Greene <wynngreene.code@gmail.com>
2023-08-18 00:38:29 -0600

    reset code

--- WEB FUND/WEEK 05 | AUG 01 - 06 (DONE)/(Prac) Video Preview/script.js
+++ WEB FUND/WEEK 05 | AUG 01 - 06 (DONE)/(Prac) Video Preview/script.js
@@ -2,8 +2,6 @@ console.log("page loaded...");
 
 // let video_Preview = document.getElementById("myVideo");
 
-function video_Play(element){
-    video_Preview = document.getElementById("myVideo").duration;
-    document.getElementById("myVideo").innerHTML = video_Preview;
-
+function videoPlay(){
+    
 }
