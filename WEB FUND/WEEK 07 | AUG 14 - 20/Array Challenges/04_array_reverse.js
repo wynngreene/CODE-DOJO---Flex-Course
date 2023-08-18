7f08f52d9621d7ae7a58c66d49051d2c371b1ecd
Wynn Greene <wynngreene.code@gmail.com>
2023-08-18 00:13:08 -0600

    update files for assignment

---
+++ WEB FUND/WEEK 07 | AUG 14 - 20/Array Challenges/04_array_reverse.js
@@ -0,0 +1,9 @@
+function betterThanAverage(arr) {
+    var sum = 0;
+    // calculate the average
+    var count = 0
+    // count how many values are greated than the average
+    return count;
+}
+var result = betterThanAverage([6, 8, 3, 10, -2, 5, 9]);
+console.log(result); // we expect back 4
---
+++ WEB FUND/WEEK 07 | AUG 14 - 20/Array Challenges/05_fibonacci_array.js
@@ -0,0 +1,9 @@
+function fibonacciArray(n) {
+    // the [0, 1] are the starting values of the array to calculate the rest from
+    var fibArr = [0, 1];
+    // your code here
+    return fibArr;
+}
+   
+var result = fibonacciArray(10);
+console.log(result); // we expect back [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
