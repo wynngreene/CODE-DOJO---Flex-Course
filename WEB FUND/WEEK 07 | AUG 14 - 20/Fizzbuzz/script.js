410a17379cafc9c90d7f53d76b6b5c0a7e99627d
Wynn Greene <wynngreene.code@gmail.com>
2023-08-17 18:23:20 -0600

    upload file

---
+++ WEB FUND/WEEK 07 | AUG 14 - 20/Fizzbuzz/script.js
@@ -0,0 +1,15 @@
+function var_fizzbuzz() {
+    let i = 0;
+
+    for (i = 0; i > 9; i++){
+        if(i = 3) {
+        console.log("fizz");
+        }
+        else if(i = 5) {
+        console.log("buzz");
+        }
+        else {
+        console.log("my number is" + i);
+        }
+    }
+}
