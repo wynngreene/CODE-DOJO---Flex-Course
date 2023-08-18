085cd2fc9598ff4ee14ebbe8bc3c512da1275ca4
Wynn Greene <wynngreene.code@gmail.com>
2023-08-18 00:03:31 -0600

    upload completed challenges

--- WEB FUND/WEEK 07 | AUG 14 - 20/Loop Challenges/script.js
+++ WEB FUND/WEEK 07 | AUG 14 - 20/Loop Challenges/script.js
@@ -4,12 +4,12 @@
 // 1. Print odds 1-20
 // Using a loop write code that will console.log all of the odd values from 1 up to 20.
 
-// function printOdds() {
-//     for (let i = 1; i < 21; i++){
-//         if (i % 2 != 0)
-//         console.log(i);
-//     }
-// }
+function printOdds() {
+    for (let i = 1; i < 21; i++){
+        if (i % 2 != 0)
+        console.log(i);
+    }
+}
 // printOdds();
 
 
@@ -30,16 +30,16 @@ decreaseByThree()
 // 3. Print the sequence
 // Using a loop write code that will console.log the values in this sequence 4, 2.5, 1, -0.5, -2, -3.5.
 
-// function printSeq() {
-//     const myArray = [4, 2.5, 1, -0.5, -2, -3.5];
-//     let = currentNum = 0;
-//     for (i = 0; i < myArray.length; i++){
+function printSeq() {
+    const myArray = [4, 2.5, 1, -0.5, -2, -3.5];
+    let = currentNum = 0;
+    for (i = 0; i < myArray.length; i++){
 
-//         currentNum = myArray[i];
+        currentNum = myArray[i];
         
-//         console.log(currentNum);
-//     }
-// }
+        console.log(currentNum);
+    }
+}
 // printSeq();
 
 // 4. Sigma
@@ -48,23 +48,23 @@ decreaseByThree()
 // function addSigma() {
 //     totalSum = 0;
 
-//     for (i = 1; i < 101; i++){
-//         totalSum = totalSum + i;
-//         console.log(totalSum);
-//     }
-// }
+    for (i = 1; i < 101; i++){
+        totalSum = totalSum + i;
+        console.log(totalSum);
+    }
+
 // addSigma();
 
 // 5. Factorial
 // Write code that will multiply all of the values from 1-12 onto some variable product and
 // at the end console.log the result 1 * 2 * 3 * ... * 10 * 11 * 12. We should get back 479001600 at the end.
 
-// function multiByFive() {
+function multiByFive() {
 
-//     currentValue = 1;
-//     for(i = 1; i < 13; i++){
-//         currentValue = currentValue * i;
-//         console.log(currentValue);
-//     }
-// }
+    currentValue = 1;
+    for(i = 1; i < 13; i++){
+        currentValue = currentValue * i;
+        console.log(currentValue);
+    }
+}
 // multiByFive();
