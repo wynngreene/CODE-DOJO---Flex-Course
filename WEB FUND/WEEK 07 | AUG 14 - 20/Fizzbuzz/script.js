7a1beb28eb816fce612d457cf789f588bd79e8c9
Wynn Greene <wynngreene.code@gmail.com>
2023-08-17 23:18:42 -0600

    upload files

--- WEB FUND/WEEK 07 | AUG 14 - 20/Fizzbuzz/script.js
+++ WEB FUND/WEEK 07 | AUG 14 - 20/Fizzbuzz/script.js
@@ -1,15 +1,21 @@
-function var_fizzbuzz() {
-    let i = 0;
+console.log("connected");
 
-    for (i = 0; i > 9; i++){
-        if(i = 3) {
-        console.log("fizz");
+function countFizzBuzz() {
+
+    for (let i = 1; i < 100; i++) {
+
+        if (i % 3 == 0 && i % 5 == 0) {
+            console.log("FizzBuzz");
         }
-        else if(i = 5) {
-        console.log("buzz");
+        else if(i % 3 == 0) {
+            console.log("Fizz");
+        }
+        else if(i % 5 == 0) {
+            console.log("Buzz");
         }
         else {
-        console.log("my number is" + i);
+        console.log(i);
         }
     }
 }
+countFizzBuzz();
---
+++ WEB FUND/WEEK 07 | AUG 14 - 20/Loop Challenges/script.js
@@ -0,0 +1,23 @@
+// owing challenges and upload your work when done.
+
+// 1. Print odds 1-20
+// Using a loop write code that will console.log all of the odd values from 1 up to 20.
+
+
+// 2. Decreasing Multiples of 3
+// Using a loop write code that will console.log all of the values that are 
+// evenly divisible by 3 from 100 down to 0.
+
+
+// 3. Print the sequence
+// Using a loop write code that will console.log the values in this sequence 4, 2.5, 1, -0.5, -2, -3.5.
+
+
+// 4. Sigma
+// Write code that will add all of the values from 1-100 onto some variable sum and
+// at the end console.log the result 1 + 2 + 3 + ... + 98 + 99 + 100. We should get back 5050 at the end.
+
+
+// 5. Factorial
+// Write code that will multiply all of the values from 1-12 onto some variable product and
+// at the end console.log the result 1 * 2 * 3 * ... * 10 * 11 * 12. We should get back 479001600 at the end.
