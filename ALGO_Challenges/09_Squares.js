50f1aa5c3abe19738dcc5477fa78810d0c7c8587
Wynn Greene <wynngreene.code@gmail.com>
2023-08-19 22:12:44 -0600

    algo challenge 09 done

--- ALGO_Challenges/09_Squares.js
+++ ALGO_Challenges/09_Squares.js
@@ -2,6 +2,14 @@
 //(e.g. [1,5,10,-2] will become [1,25,100,4])
 
 function squareVal(arr) {
-    //your code here 
-    return arr; 
+    let holdMe = 0;
+
+    for(let i = 0; i < arr.length; i++){
+        holdMe = arr[i]*arr[i];
+        arr[i] = holdMe;
+    }
+    console.log(arr);
+
 }
+
+squareVal([1,3,5,7,9])
