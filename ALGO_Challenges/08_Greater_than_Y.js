b65098a3762adf75decb2bc4f4abf6c1a6292678
Wynn Greene <wynngreene.code@gmail.com>
2023-08-19 22:05:01 -0600

    Update 08_Greater_than_Y.js

--- ALGO_Challenges/08_Greater_than_Y.js
+++ ALGO_Challenges/08_Greater_than_Y.js
@@ -2,6 +2,18 @@
 //For example if arr = [1, 3, 5, 7] and Y = 3, your function will return 2.
 //(There are two values in the array greater than 3, which are 5, 7).
 function greaterY(arr, Y) {
-    //your code here 
+    let count = 0;
+    let thatsMine = Y;
+
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] > thatsMine){
+            count++;
+        }
+    }
+    console.log(thatsMine);
+    console.log(count);
+
     return count; 
 }
+
+greaterY([1,4,4,5], 4)
