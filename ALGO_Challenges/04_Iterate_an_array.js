41c5c54c64c0590bfd6723d3869f631a2648f37b
Wynn Greene <wynngreene.code@gmail.com>
2023-08-19 21:05:32 -0600

    algo challenge 04 done

--- ALGO_Challenges/04_Iterate_an_array.js
+++ ALGO_Challenges/04_Iterate_an_array.js
@@ -1,11 +1,21 @@
-//Write a function that returns the sum of all the values within an array. (e.g. [1,2,5] returns 8, [-5,2,5,12] returns 14)
+// //Write a function that returns the sum of all the values within an array. (e.g. [1,2,5] returns 8, [-5,2,5,12] returns 14)
+// function iterArr(arr) {
+//     //your code here 
+
+//     let arr = [1,2,5]
+
+//     for(i = 0; i == arr.length; i++){
+//         arr  
+//     }
+//     return sum; 
+// }
 function iterArr(arr) {
-    //your code here 
+    let sum = 0;
 
-    let arr = [1,2,5]
-
-    for(i = 0; i == arr.length; i++){
-        arr  
+    for(let i = 0;i < arr.length; i++){
+        sum = arr[i] + sum;
     }
-    return sum; 
+    console.log(sum);
 }
+
+iterArr([1,2,3]);
