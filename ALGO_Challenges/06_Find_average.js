c28d0b1f15ee89db77fd4251b33e5b3cd6e26e40
Wynn Greene <wynngreene.code@gmail.com>
2023-08-19 21:47:18 -0600

    algo challenge 06 done

--- ALGO_Challenges/06_Find_average.js
+++ ALGO_Challenges/06_Find_average.js
@@ -1,14 +1,31 @@
 //Given an array with multiple values,write a function that returns the average of the values in the array.
 //(e.g. for [1,3,5,7,20] average is 7.2)
-let max = 0;
-let averageArr = 0;
+// let max = 0;
+// let averageArr = 0;
 
-function findMax(arr) {
-    for (i=0; i == arr.length; i++){
-    max = arr[i] + max;
+// function findMax(arr) {
+//     for (i=0; i == arr.length; i++){
+//     max = arr[i] + max;
 
+//     }
+//     averageArr = max
+//     //your code here 
+//     return max; 
+// }
+
+function findAvg(arr) {
+    let sum = 0;
+    let myArrayLength = arr.length;
+
+    for(let i = 0; i < arr.length; i++){
+        sum = arr[i] + sum;
     }
-    averageArr = max
-    //your code here 
-    return max; 
+
+    console.log(sum);
+    console.log(myArrayLength);
+
+    let avg = sum / myArrayLength;
+    console.log(avg);
 }
+
+findAvg([1,2,3,4,5]);
