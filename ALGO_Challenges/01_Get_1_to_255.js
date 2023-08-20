c7aa85ffe69fc9080aa0c759fd77c9e67100ea75
Wynn Greene <wynngreene.code@gmail.com>
2023-08-19 20:35:05 -0600

    algo challenge 01 done

--- ALGO_Challenges/01_Get_1_to_255.js
+++ ALGO_Challenges/01_Get_1_to_255.js
@@ -1,12 +1,23 @@
 //Write a function that returns an array with all the numbers from 1 to 255. You may use the push() function for this exercise.
-function get_array(startNumb,finishNumb) {
-    var arr = [];
+// function get_array(startNumb,finishNumb) {
+//     var arr = [];
+//     for (var i = startNumb ; i = finishNumb; i++)
+//     {
+//         arr += arr.push(i);
+//     }
+//     return arr;
+// };
+// console.log(get_array(2,10));
 
-    for (var i = startNumb ; i = finishNumb; i++)
-    {
-        arr += arr.push(i);
+function get_array() {
+    let arr = [];
+    let numbEnd = 10;
+
+    for(let i = 1; i < numbEnd; i++){
+        i = arr.push(i);
+
     }
-    return arr;
-};
+    console.log(arr);
 
-console.log(get_array(2,10));
+}
+get_array();
--- WEB FUND/WEEK 07 | AUG 14 - 20/Array Challenges/02_high_Pass_filter.js
+++ WEB FUND/WEEK 07 | AUG 14 - 20/Array Challenges/02_high_Pass_filter.js
@@ -1,8 +1,7 @@
-function alwaysHungry(arr) {
-    // your code here 
+function highPass(arr, cutoff) {
+    var filteredArr = [];
+    // your code here
+    return filteredArr;
 }
-   
-alwaysHungry([3.14, "food", "pie", true, "food"]);
-// this should console log "yummy", "yummy"
-alwaysHungry([4, 1, 5, 7, 2]);
-// this should console log "I'm hungry"
+var result = highPass([6, 8, 3, 10, -2, 5, 9], 5);
+console.log(result); // we expect back [6, 8, 10, 9]
