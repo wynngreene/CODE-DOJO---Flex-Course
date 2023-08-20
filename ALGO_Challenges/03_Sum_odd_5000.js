a5b2c8976365b31415297d804aae1cc73d92dc2b
Wynn Greene <wynngreene.code@gmail.com>
2023-08-19 20:59:23 -0600

    algo challenge 03 done

--- ALGO_Challenges/03_Sum_odd_5000.js
+++ ALGO_Challenges/03_Sum_odd_5000.js
@@ -1,14 +1,27 @@
 //Write a function that returns the sum of all the odd numbers from 1 to 5000. (e.g. 1+3+5+...+4997+4999)
+// function sum_odd_5000() {
+//     var sum = 0;
+//     //your code here
+//     let myOddArray = [];
+
+//     for (i = 0; i = 10; i++){
+//         if(i % 2 == 0){
+//         myOddArray[i] = i;
+            
+//     }
+    
+//     return sum; 
+// }
+
 function sum_odd_5000() {
     var sum = 0;
-    //your code here
-    let myOddArray = [];
-
-    for (i = 0; i = 10; i++){
-        if(i % 2 == 0){
-        myOddArray[i] = i;
-            
+    for(i = 1; i < 7; i++){
+        if(i % 2 !==0){
+            sum = i + sum;
+        }
     }
-    
-    return sum; 
+
+    console.log(sum);
 }
+
+sum_odd_5000();
