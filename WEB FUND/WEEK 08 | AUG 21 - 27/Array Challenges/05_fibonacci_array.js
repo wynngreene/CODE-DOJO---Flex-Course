bbed29b43cefba3f4e6944f0aa506606da339e22
Wynn Greene <wynngreene.code@gmail.com>
2023-08-24 00:29:29 -0600

    05 Array Challenge done

--- WEB FUND/WEEK 08 | AUG 21 - 27/Array Challenges/05_fibonacci_array.js
+++ WEB FUND/WEEK 08 | AUG 21 - 27/Array Challenges/05_fibonacci_array.js
@@ -1,11 +1,15 @@
-//PROGRESS
+//DONE
 
 function fibonacciArray(n) {
-    // the [0, 1] are the starting values of the array to calculate the rest from
+
     var fibArr = [0, 1];
-    // your code here
+
+    for (let i = 0; i < n; i++){
+        fibArr[i+2] = fibArr[i] + fibArr[i + 1];
+    }
+    console.log(fibArr);
     return fibArr;
 }
-   
-var result = fibonacciArray(10);
-console.log(result); // we expect back [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+// var result = fibonacciArray(10);
+// console.log(result); // we expect back [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+fibonacciArray(10);
