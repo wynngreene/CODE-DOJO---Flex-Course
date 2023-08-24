a1d0f89ddd3f2712be86e695795cccba5c03f65b
Wynn Greene <wynngreene.code@gmail.com>
2023-08-23 23:56:04 -0600

    02 array challenge done

--- WEB FUND/WEEK 08 | AUG 21 - 27/Array Challenges/02_high_Pass_filter.js
+++ WEB FUND/WEEK 08 | AUG 21 - 27/Array Challenges/02_high_Pass_filter.js
@@ -1,7 +1,15 @@
 function highPass(arr, cutoff) {
+
     var filteredArr = [];
-    // your code here
+    for (let i = 0;i < arr.length; i++){
+        if(arr[i] > cutoff){
+            filteredArr.push(arr[i]);
+        }
+    }
+    console.log(filteredArr);
     return filteredArr;
 }
-var result = highPass([6, 8, 3, 10, -2, 5, 9], 5);
-console.log(result); // we expect back [6, 8, 10, 9]
+// var result = highPass([6, 8, 3, 10, -2, 5, 9], 5);
+// console.log(result); // we expect back [6, 8, 10, 9]
+
+highPass([6, 8, 3, 10, -2, 5, 9], 5);
