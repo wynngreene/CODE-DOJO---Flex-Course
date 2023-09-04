5c83420bbdaa5a5deee578021d7018c3ebd5b2e8
Wynn Greene <wynngreene.code@gmail.com>
2023-09-03 22:12:29 -0600

    upload changes

---
+++ 03 | PYTHON/WEEK 01/(PRAC) Hello World/hello_world.py
@@ -0,0 +1,19 @@
+# 1. TASK: print "Hello World"
+print("Hello World")
+
+# 2. print "Hello Noelle!" with the name in a variable
+name = "Wynn"
+print("Hello,{{name}}!")	# with a comma
+print("Hello, +{{name}}!")	# with a +
+
+# 3. print "Hello 42!" with the number in a variable
+num = 42
+print("Hello,{{num}}!")	# with a comma
+print("Hello, + --")	# with a +	-- this one should give us an error!
+
+# 4. print "I love to eat sushi and pizza." with the foods in variables
+# fave_food1 = "sushi"
+# fave_food2 = "pizza"
+# print( your code here ) # with .format()
+# print( your code here ) # with an f string
+
---
+++ 03 | PYTHON/WEEK 01/For Loops Basic I/for_loop_basic1.py
@@ -0,0 +1,25 @@
+#01. Basic - Print all integers from 0 to 150.
+def function_Basic():
+for x in :
+    print()
+
+function_Basic()
+
+
+#02. Multiples of Five - Print all the multiples of 5 from 5 to 1,000
+
+
+#03. Counting, the Dojo Way - Print integers 1 to 100.
+#    If divisible by 5, print "Coding" instead. If divisible by 10, print "Coding Dojo".
+
+
+#04. Whoa. That Sucker's Huge - Add odd integers from 0 to 500,000, and print the final sum.
+
+
+#05. Countdown by Fours - Print positive numbers starting at 2018, counting down by fours.
+
+
+#06. Flexible Counter - Set three variables: lowNum, highNum, mult.
+#    Starting at lowNum and going through highNum, print only the integers that are a multiple of mult.
+#    For example, if lowNum=2, highNum=9, and mult=3, the loop should print 3, 6, 9 (on successive lines).
+
