d2b86ff634fb5eeb1283ee82523f48a69d171be8
Wynn Greene <wynngreene.code@gmail.com>
2023-09-11 01:59:59 -0600

    Update nested_dict_and_list.py

--- 03_PYTHON/WEEK_02/nested_dictionaries_and_lists/nested_dict_and_list.py
+++ 03_PYTHON/WEEK_02/nested_dictionaries_and_lists/nested_dict_and_list.py
@@ -1,4 +1,4 @@
-print("connected")
+# print("connected")
 
 #01. CODE
 x = [ [5,2,3], [10,8,9] ] 
@@ -24,16 +24,59 @@ print(sports_directory["soccer"][0])
 z[0]["y"] = 30
 print(z[0]["y"])
 
-#02. CODE
-#02.Iterate Through a List of Dictionaries
 
+#02. CODE
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+#02.Iterate Through a List of Dictionaries
+def iterateDictionary(some_list):
+    for x in range(0,len(students),1):
+        print("first_name - " + students[x]["first_name"] +",","last_name - " + students[x]["last_name"])
+
+iterateDictionary(students)
 
 
 #03. CODE
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
 #03.Get Values From a List of Dictionaries
+def iterateDictionary2(key_name, some_list):
+    if key_name == "first_name":
+        for x in range(0,len(students),1):
+            print(students[x]["first_name"])
+    if key_name == "last_name":
+        for x in range(0,len(students),1):
+                print(students[x]["last_name"])
 
-
+iterateDictionary2("first_name", students)
+iterateDictionary2("last_name", students)
 
 
 #04. CODE
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
 #04.Iterate Through a Dictionary with List Values
+def printInfo(some_dict):
+    num_Loc = len(dojo["locations"])
+    print(str(num_Loc) + " LOCATIONS")
+    for x in range(0,num_Loc,1):
+        print(dojo["locations"][x])
+
+    print("-------------")
+
+    num_Instr = len(dojo["instructors"])
+    print(str(num_Instr) + " INSTRUCTORS")
+    for x in range(0,num_Instr,1):
+        print(dojo["instructors"][x])
+
+printInfo(dojo)
