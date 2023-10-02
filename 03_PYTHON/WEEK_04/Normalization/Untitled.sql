b09c33e43f95d0bd46cd8eb034871b1d9495ebd3
Wynn Greene <wynngreene.code@gmail.com>
2023-10-02 01:02:40 -0600

    upload sql files

--- 03_PYTHON/WEEK_04/Normalization/Untitled.sql
+++ 03_PYTHON/WEEK_04/Normalization/Untitled.sql
@@ -1,1 +1,4 @@
-SELECT * FROM dojos;
+SELECT ninjas.first_name, ninjas.last_name, dojos.name
+FROM ninjas
+JOIN dojos ON dojos.id = ninjas.dojo_id
+WHERE ninjas_id = 6;
---
+++ 03_PYTHON/WEEK_05/Books SQL/books.sql
@@ -0,0 +1,25 @@
+-- Query: Create 5 different users: Jane Amsden, Emily Dixon, Theodore Dostoevsky, William Shapiro, Lao Xiu
+
+-- Query: Create 5 books with the following names: C Sharp, Java, Python, PHP, Ruby
+
+-- Query: Change the name of the C Sharp book to C#
+
+-- Query: Change the first name of the 4th user to Bill
+
+-- Query: Have the first user favorite the first 2 books
+
+-- Query: Have the second user favorite the first 3 books
+
+-- Query: Have the third user favorite the first 4 books
+
+-- Query: Have the fourth user favorite all the books
+
+-- Query: Retrieve all the users who favorited the 3rd book
+
+-- Query: Remove the first user of the 3rd book's favorites
+
+-- Query: Have the 5th user favorite the 2nd book
+
+-- Find all the books that the 3rd user favorited
+
+-- Query: Find all the users that favorited to the 5th book
