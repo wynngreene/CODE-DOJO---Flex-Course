2c0e60933a75e52e8ea567927e82a894bc67d115
Wynn Greene <wynngreene.code@gmail.com>
2023-10-06 03:01:17 -0600

    update changes

--- 03_PYTHON/WEEK_06/Playground/Flask_playground/static/scripts.js
+++ 03_PYTHON/WEEK_06/Playground/Flask_playground/static/scripts.js
@@ -1,6 +1,6 @@
 console.log("connected_outside");
 
-// function myColorChangeInside(color){
-//     console.log("connected_inside");
-//     document.getElementById("change_color").innerHTML = "<p>BOX2</p>";
-// }
+function myColorChangeInside(color){
+    console.log("connected_inside");
+    document.getElementById("change_color").innerHTML = "<p>BOX_02</p>";
+}
