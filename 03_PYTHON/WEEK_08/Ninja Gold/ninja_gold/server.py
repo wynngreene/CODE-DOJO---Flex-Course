c151ac00a0e823be0da956cd06fb7dce28271329
Wynn Greene <wynngreene.code@gmail.com>
2023-10-23 02:02:57 -0600

    upload files

--- 03_PYTHON/WEEK_08/Ninja Gold/ninja_gold/server.py
+++ 03_PYTHON/WEEK_08/Ninja Gold/ninja_gold/server.py
@@ -1,19 +1,44 @@
-from flask import Flask, render_template# Import Flask to allow us to create our app
-app = Flask(__name__)    # Create a new instance of the Flask class called "app"
-# START CAP
+from flask import Flask, render_template, session, redirect, request
+app = Flask(__name__)
+app.secret_key = 'secret_safe.'
+
+#Global Stuff 
 
 
-# 01 PAGE
-@app.route('/')          
+# 01 PAGE 
+@app.route('/')                           
 def index():
-
-    return render_template("index.html")
-
+    print(session.get("gold")) 
+    return render_template('index.html')
 
 # 02 PAGE
-# @app.route('/destroy_session')          
-# def destroy_session():
-#     return render_template("index.html")  
+@app.route('/process_money', methods=["POST"])          
+def process_money():
+    
+    if request.form["building"] == "farm":
+        print("my gold from farm!")
+        session["gold"] += 20
+        print(session.get("gold"))
+        
+    elif request.form["building"] == "cave":
+        print("my gold from cave!")
+        session["gold"] += 10
+        print(session.get("gold"))       
+        
+    elif request.form["building"] == "house":
+        print("my gold from house!")
+        session["gold"] += 5
+        print(session.get("gold"))
+                
+    elif request.form["building"] == "casino":
+        print("my gold from casino!")
+        session["gold"] -= 50
+        print(session.get("gold"))
+                
+    else:
+        print("my gold is changing!")
+    
+    return redirect ('/') 
 
 # # 03 PAGE
 # @app.route('/users', methods=['POST'])
@@ -28,6 +53,7 @@ def index():
 # @app.route('/show')
 # def show_user():
 #     return render_template('show.html', name_on_template=session['username'], email_on_template=session['useremail'])
+
 
 # END CAP
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
