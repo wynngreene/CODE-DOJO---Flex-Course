4ca6688a1e09b61d29a854018905cd7834e8c26e
Wynn Greene <wynngreene.code@gmail.com>
2023-10-24 20:23:41 -0600

    update files

--- 03_PYTHON/WEEK_08/Ninja Gold/ninja_gold/server.py
+++ 03_PYTHON/WEEK_08/Ninja Gold/ninja_gold/server.py
@@ -8,7 +8,9 @@ app.secret_key = 'secret_safe.'
 # 01 PAGE 
 @app.route('/')                           
 def index():
-    print(session.get("gold")) 
+    print(session.get("gold"))
+    print(session.get("activity"))
+    
     return render_template('index.html')
 
 # 02 PAGE
@@ -16,14 +18,17 @@ def index():
 def process_money():
     
     if request.form["building"] == "farm":
-        print("my gold from farm!")
         session["gold"] += 20
+        session["activity"] += "<ul><li>WAHT</li></ul>"
+        
+        print("my gold from farm!")
         print(session.get("gold"))
+        
         
     elif request.form["building"] == "cave":
         print("my gold from cave!")
         session["gold"] += 10
-        print(session.get("gold"))       
+        print(session.get("gold"))
         
     elif request.form["building"] == "house":
         print("my gold from house!")
@@ -39,21 +44,6 @@ def process_money():
         print("my gold is changing!")
     
     return redirect ('/') 
-
-# # 03 PAGE
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     print("Got Post Info")
-#     print(request.form)
-#     session['username'] = request.form['name']
-#     session['useremail'] = request.form['email']
-#     return redirect('/show')
-
-# # 04 PAGE
-# @app.route('/show')
-# def show_user():
-#     return render_template('show.html', name_on_template=session['username'], email_on_template=session['useremail'])
-
 
 # END CAP
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
