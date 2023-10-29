ca58cec3800b37cea8e2e686e23ecea0bde35a68
Wynn Greene <wynngreene.code@gmail.com>
2023-10-28 22:38:17 -0600

    update files

--- 03_PYTHON/WEEK_08/Ninja Gold/ninja_gold/server.py
+++ 03_PYTHON/WEEK_08/Ninja Gold/ninja_gold/server.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 from flask import Flask, render_template, session, redirect, request
 app = Flask(__name__)
 app.secret_key = 'secret_safe.'
@@ -44,6 +45,31 @@ def process_money():
         print("my gold is changing!")
     
     return redirect ('/') 
+=======
+from flask import Flask, render_template, session, redirect, request # Import Flask to allow us to create our app
+app = Flask(__name__)    # Create a new instance of the Flask class called "app"
+app.secret_key = 'secret_safe.'
+# START CAP
+
+
+# 01 PAGE
+@app.route('/')          
+def index():
+    return render_template("index.html")
+
+@app.route('/process', methods=["POST"])
+def process():
+    session["fullname"] = request.form["fullname"]
+    session["location"] = request.form["location"]
+    session["language"] = request.form["language"]
+    session["comments"] = request.form["comments"]
+    return redirect ('/results')
+
+@app.route('/results')
+def results():
+    return render_template("results.html")
+
+>>>>>>> Stashed changes
 
 # END CAP
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
