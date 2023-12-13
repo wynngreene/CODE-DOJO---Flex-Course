a4c5d77bddca45460b177a7a3a30e9c2caf12949
Wynn Greene <wynngreene.code@gmail.com>
2023-12-13 01:13:49 -0700

    Register feature works

--- 03_PYTHON/WEEK_13/Login and Registration/flask_app/controllers/users_Controller.py
+++ 03_PYTHON/WEEK_13/Login and Registration/flask_app/controllers/users_Controller.py
@@ -1,8 +1,8 @@
 from flask import render_template, redirect, session, request
 from flask_app import app
 from flask_app.models.user_Model import User
-from flask_bcrypt import Bcrypt
-bcrypt = Bcrypt(app)
+from flask_bcrypt import Bcrypt        
+bcrypt = Bcrypt(app) 
 
 # GET ROUTES
 @app.route("/")
@@ -10,10 +10,10 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/register", method=["POST"])
+@app.route("/register", methods=["POST"])
 def register():
     
-    if not User.is_valid(request.form):
+    if not User.validate_register(request.form):
         return redirect("/")
     data = {
         "first_name" : request.form["first_name"],
@@ -27,16 +27,19 @@ def register():
     return redirect("/dashboard")
 
 
-@app.route("/login", method=["POST"])
+@app.route("/login", methods=["POST"])
 def login():
     pass
 
 
 @app.route("/dashboard")
 def dashboard():
-    if "user.id" not in session:
-        redirect("/logout")
-
+    if "user_id" not in session:
+        return redirect("/logout")
+    data = {
+        "id" : session["user_id"]
+    }
+    return render_template("dashboard.html", user=User.get_by_id(data))
 
 @app.route("/logout")
 def logout():
--- 03_PYTHON/WEEK_13/Login and Registration/flask_app/models/user_Model.py
+++ 03_PYTHON/WEEK_13/Login and Registration/flask_app/models/user_Model.py
@@ -20,9 +20,20 @@ class User:
     def save(cls, data):
         query = """
                 INSERT INTO users ( first_name, last_name, email, password, created_at, updated_at )
-                VALUES ( %(first_name)s, %(last_name)s, %(email)s, %(password)s, NOW(),  NOW() );"""
+                VALUES ( %(first_name)s, %(last_name)s, %(email)s, %(password)s, NOW(),  NOW() );
+                """
         save_result = connectToMySQL(cls.DB).query_db(query, data)
         return save_result
+
+#---GET_ALL_USER---
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM users"
+        get_all_results = connectToMySQL(cls.DB).query_db(query)
+        users = []
+        for row in get_all_results:
+            users.append( cls[row])
+        return users
 
 #---GET_BY_EMAIL---DONE
     @classmethod
@@ -30,6 +41,13 @@ class User:
         query = "SELECT * FROM users WHERE email = %(email)s;"
         email_results = connectToMySQL(cls.DB).query_db(query, data)
         return cls(email_results[0])
+
+#---GET_BY_ID---DONE
+    @classmethod
+    def get_by_id(cls, data):    
+        query = "SELECT * FROM users WHERE id = %(id)s;"
+        id_results = connectToMySQL(cls.DB).query_db(query, data)
+        return cls(id_results[0])
 
 #---VALIDATE_EMAIL---DONE
     @staticmethod
@@ -77,44 +95,7 @@ class User:
         is_valid = True
         query = "SELECT * FROM users WHERE email = %(email)s;"
         valid_login_results = connectToMySQL(User.DB).query_db(query, user)
-
         return is_valid
 
-
-# #---GET_ONE_USER---
-#     @classmethod
-#     def get_all(cls):
-#         query = "SELECT * FROM cookie_orders"
-#         get_all_results = connectToMySQL(cls.DB).query_db(query)
-#         users = []
-#         for row in get_all_results:
-#             users.append( cls[row])
-#         return users
-
-
-# #---GET_ONE---
-#     @classmethod
-#     def get_one(cls, order_id):
-#         query = """
-#                 SELECT * FROM users
-#                 WHERE id = %(id)s
-#                 """
-#         order_dictionary = connectToMySQL(cls.DB).query_db(query, {"id": order_id})
-#         order = Cookie_order(order_dictionary[0])
-#         return order
-
-# #---EDIT---
-#     @classmethod
-#     def edit(cls, data):
-#         query = """
-#                 UPDATE cookie_orders
-#                 SET full_name = %(full_name)s,
-#                 cookie_type = %(cookie_type)s,
-#                 number_boxes = %(number_boxes)s,
-#                 updated_at = NOW()
-#                 WHERE id = %(id)s;
-#                 """
-#         result = connectToMySQL(cls.DB).query_db(query, data)
-#         return result
     
 
