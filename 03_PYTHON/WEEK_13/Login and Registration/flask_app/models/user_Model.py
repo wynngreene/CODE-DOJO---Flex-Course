e806a30d9199985e1165c382c7e93e434312e316
Wynn Greene <wynngreene.code@gmail.com>
2023-12-12 23:27:28 -0700

    upload model file

--- 03_PYTHON/WEEK_13/Login and Registration/flask_app/models/user_Model.py
+++ 03_PYTHON/WEEK_13/Login and Registration/flask_app/models/user_Model.py
@@ -4,74 +4,105 @@ EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 from flask import flash
 
 class User:
+#---START---DONE
     DB = "login_reg"
     def __init__(self, data):
         self.id = data["id"]
-        self.full_name = data["full_name"]
-        self.cookie_type = data["cookie_type"]
-        self.number_boxes = data["number_boxes"]
+        self.first_name = data["first_name"]
+        self.last_name = data["last_name"]
+        self.email = data["email"]
+        self.password = data["password"]
         self.created_at = data["created_at"]
         self.updated_at = data["updated_at"]
-        
-    @classmethod
-    def get_one(cls, order_id):
-        query = """
-                SELECT * FROM cookie_orders
-                WHERE id = %(id)s
-                """
-        order_dictionary = connectToMySQL(cls.DB).query_db(query, {"id": order_id})
-        order = Cookie_order(order_dictionary[0])
-        return order
 
-    @classmethod
-    def get_all(cls):
-        all_orders_list = []
-        query = "SELECT * FROM cookie_orders"
-        all_orders_data = connectToMySQL(cls.DB).query_db(query)
-        
-        for order in all_orders_data:
-            new_order_object = Cookie_order(order)
-            all_orders_list.append(new_order_object)
-        return all_orders_list
-    
+#---SAVE---DONE
     @classmethod
     def save(cls, data):
         query = """
-                INSERT INTO cookie_orders ( full_name, cookie_type, number_boxes, created_at, updated_at )
-                VALUES ( %(full_name)s, %(cookie_type)s, %(number_boxes)s, NOW(),  NOW() );"""
-        result = connectToMySQL(cls.DB).query_db(query, data)
-        return result
-    
+                INSERT INTO users ( first_name, last_name, email, password, created_at, updated_at )
+                VALUES ( %(first_name)s, %(last_name)s, %(email)s, %(password)s, NOW(),  NOW() );"""
+        save_result = connectToMySQL(cls.DB).query_db(query, data)
+        return save_result
 
+#---GET_BY_EMAIL---DONE
     @classmethod
-    def edit(cls, data):
-        query = """
-                UPDATE cookie_orders
-                SET full_name = %(full_name)s,
-                cookie_type = %(cookie_type)s,
-                number_boxes = %(number_boxes)s,
-                updated_at = NOW()
-                WHERE id = %(id)s;
-                """
-        result = connectToMySQL(cls.DB).query_db(query, data)
-        return result
-    
-    @staticmethod
-    def validate_order(data):
-        is_valid = True
-        # Check if each field is blank
-        if len(data["full_name"]) == 0:
-            flash("Name required!")
-            is_valid = False
-        if len(data["cookie_type"]) == 0:
-            flash("Cookie type required!")
-            is_valid = False
-        if len(data["number_boxes"]) == 0:
-            flash("Number of box(es) required!")
-            is_valid = False    
-        elif int(data["number_boxes"]) <= 0:
-            flash("Number of box(es) must be a valid positive!")
-            is_valid = False
+    def get_by_email(cls, data):    
+        query = "SELECT * FROM users WHERE email = %(email)s;"
+        email_results = connectToMySQL(cls.DB).query_db(query, data)
+        return cls(email_results[0])
 
+#---VALIDATE_EMAIL---DONE
+    @staticmethod
+    def validate_register(user):
+        is_valid = True
+        query = "SELECT * FROM users WHERE email = %(email)s;"
+        valid_email_results = connectToMySQL(User.DB).query_db(query, user)
+        if len(valid_email_results) >= 1:
+            flash("Email already taken")
+            is_valid = False
+        if not EMAIL_REGEX.match(user["email"]):
+            flash("Invalid Email!!!")
+            is_valid = False
         return is_valid
+
+#---VALIDATE_EMAIL---DONE
+    @staticmethod
+    def validate_register(user):
+        is_valid = True
+        query = "SELECT * FROM users WHERE email = %(email)s;"
+        valid_email_results = connectToMySQL(User.DB).query_db(query, user)
+        if len(valid_email_results) >= 1:
+            flash("Email already taken")
+            is_valid = False
+        if not EMAIL_REGEX.match(user["email"]):
+            flash("Invalid Email!!!")
+            is_valid = False
+        return is_valid
+
+#---VALIDATE_LOGIN---DONE
+    @staticmethod
+    def validate_login(user):
+        is_valid = True
+        query = "SELECT * FROM users WHERE email = %(email)s;"
+        valid_login_results = connectToMySQL(User.DB).query_db(query, user)
+        
+        return is_valid
+
+
+# #---GET_ONE_USER---
+#     @classmethod
+#     def get_all(cls):
+#         query = "SELECT * FROM cookie_orders"
+#         get_all_results = connectToMySQL(cls.DB).query_db(query)
+#         users = []
+#         for row in get_all_results:
+#             users.append( cls[row])
+#         return users
+
+
+# #---GET_ONE---
+#     @classmethod
+#     def get_one(cls, order_id):
+#         query = """
+#                 SELECT * FROM users
+#                 WHERE id = %(id)s
+#                 """
+#         order_dictionary = connectToMySQL(cls.DB).query_db(query, {"id": order_id})
+#         order = Cookie_order(order_dictionary[0])
+#         return order
+
+# #---EDIT---
+#     @classmethod
+#     def edit(cls, data):
+#         query = """
+#                 UPDATE cookie_orders
+#                 SET full_name = %(full_name)s,
+#                 cookie_type = %(cookie_type)s,
+#                 number_boxes = %(number_boxes)s,
+#                 updated_at = NOW()
+#                 WHERE id = %(id)s;
+#                 """
+#         result = connectToMySQL(cls.DB).query_db(query, data)
+#         return result
+    
 
