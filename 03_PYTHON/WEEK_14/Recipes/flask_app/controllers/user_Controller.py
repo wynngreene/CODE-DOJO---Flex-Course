7aa4544ce3a65df3ed49a119311d64fc889aa620
Wynn Greene <wynngreene.code@gmail.com>
2023-12-15 19:42:06 -0700

    upload changes from controller

--- 03_PYTHON/WEEK_14/Recipes/flask_app/controllers/user_Controller.py
+++ 03_PYTHON/WEEK_14/Recipes/flask_app/controllers/user_Controller.py
@@ -4,7 +4,32 @@ from flask_app import app
 # from flask_bcrypt import Bcrypt        
 # bcrypt = Bcrypt(app) 
 
-# GET ROUTES
+# 00 ROUTES
 @app.route("/")
 def index():
     return render_template("index.html")
+
+# 01 ROUTES
+@app.route("/recipes")
+def dashboard():
+    return render_template("dashboard.html")
+
+# 02 ROUTES
+@app.route("/recipes/new")
+def new_recipe():
+    return redirect("/recipes")
+
+# 03 ROUTES
+@app.route("/recipes/{id}")
+def detail_recipe():
+    return redirect("/recipes")
+
+# 04 ROUTES
+@app.route("/recipes/edit")
+def edit_recipe():
+    return redirect("/recipes")
+
+# 05 ROUTES
+@app.route("/recipes/edit")
+def delete_recipe():
+    return redirect("/recipes")
