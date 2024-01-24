2a971f74fb68368abcefccce523ffe0e5b85e9db
Wynn Greene <wynngreene.code@gmail.com>
2024-01-24 13:17:16 -0700

    update changes

--- 04_ALGOS/WEEK_01/Fundamentals_To_Do_01.js
+++ 04_ALGOS/WEEK_01/Fundamentals_To_Do_01.js
@@ -1,17 +1,32 @@
 //1. Multiples of Three – but Not All
 //Using FOR, print multiples of 3 from -300 to 0. Skip -3 and -6.
 
- 
+function one_multi_Three()
+{
+
+}
 
 //2. Printing Integers with While
 //Print integers from 2000 to 5280, using a WHILE.
 
- 
+function two_integer_Print()
+{
+
+}
 
 //3. Counting, the Dojo Way
 //Print integers 1 to 100. If divisible by 5, print "Coding" instead. If by 10, also print " Dojo".
 
- 
+function three_Count_Five_Ten()
+{
+
+}
 
 //4. Flexible Countdown
 //Given lowNum, highNum, mult, print multiples of mult from highNum down to lowNum, using a FOR. For (2,9,3), print 9 6 3 (on successive lines).
+
+function four_Countdown(lowNum, highNum, multNum)
+{
+
+}
+
