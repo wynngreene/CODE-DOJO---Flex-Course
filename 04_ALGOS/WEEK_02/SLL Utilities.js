6be69c8a1b09a4499ce35ac43ee9770d916102cb
Wynn Greene <wynngreene.code@gmail.com>
2024-01-26 08:54:56 -0700

    upload changes to folders

---
+++ 04_ALGOS/WEEK_02/SLL Utilities.js
@@ -0,0 +1,26 @@
+class SLLNode {
+    constructor(val) {
+        this.value = val;
+        this.next = null;              
+    }
+}
+
+
+class SLL {
+    // constructor, other methods, removed for brevity
+    contains(value) {
+    	// is "value" to be found anywhere in this list?
+    }
+
+    // constructor, other methods, removed for brevity
+    length() {
+    	// is "value" to be found anywhere in this list?
+    }
+    // constructor, other methods, removed for brevity
+    display() {
+    // is "value" to be found anywhere in this list?
+    }
+
+
+}
+
