d4c9836e33c8101efe5d1a54c4f1ade00e7916b0
Wynn Greene <wynngreene.code@gmail.com>
2024-01-28 00:57:29 -0700

    upload solution and files

--- 04_ALGOS/WEEK_03/Trees To Do 1.js
+++ 04_ALGOS/WEEK_03/Trees To Do 1.js
@@ -1,6 +1,6 @@
 class BTNode {
     constructor(value) {
-        this.val = value;
+        this.value = value;
         this.left = null;
         this.right = null;
     }
@@ -9,31 +9,73 @@ class BST {
     constructor() {
         this.root = null;
     }
-    // add methods here...
-    add(val){
-        var newNode = new BTNode(val);
-        newNode.next = this.root;
-        this.root = newNode;
-        return this.root;
-
+    // add node to binary tree
+    add(value){
+        this.root = this.addNode(this.root,value);
     }
-    contains(val){
-        let runner = this.root;
-        while (runner){
-            if (runner.val == val){
-                return true;
-            }
-            runner = runner.next;
+
+    //nested functions
+    addNode(runner, value) {
+        if (runner === null ){
+            return new BTNode(value);
         }
-        return false;
+        if (value < runner.data){
+            runner.left = this.addNode(runner.left, value);
+        }
+        else if (value > runner.data){
+            runner.right = this.addNode(runner.right, value);
+        }
+        return runner;
     }
 
-    min(){
-
+    // check if tree contains a specific value
+    contains(value){
+        return this.containsNode(this.root, value);
     }
     
+    //nested functions
+    containsNode(runner, value){
+        if (runner == null){
+            return false;
+        }
+
+        if (value === runner.value) {
+            return true; 
+        }
+        else if (value < runner.value){
+            return this.containsNode(runner.left, value);
+        }
+        else {
+            return this.containsNode(runner.right, value);
+        }
+    }
+
+    //Return smallest value 
+    min(){
+        let runner = this.root;
+        let min_Value = this.root.value;
+
+        while (runner.left) {
+            if (runner.left.value < min_Value) {
+                min_Value = runner.left.value;
+            }
+            runner = runner.left;
+        }
+        return min_Value;
+    }
+
+    //Return largest value 
     max(){
-        
+        let runner = this.root;
+        let max_Value = this.root.value;
+
+        while (runner.right) {
+            if (runner.right.value < max_Value) {
+                max_Value = runner.right.value;
+            }
+            runner = runner.right;
+        }
+        return max_Value;        
     }
 }
 
