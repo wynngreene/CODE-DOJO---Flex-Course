9f8a683f5eeee4c903bded84578e189f5968ea52
Wynn Greene <wynngreene.code@gmail.com>
2024-01-28 01:18:05 -0700

    upload file

---
+++ 04_ALGOS/WEEK_04/Recursion To Do 1.js
@@ -0,0 +1,10 @@
+// Recursive Sigma | 
+// Write a recursive function that given a number returns the sum of integers from 1 to that number.
+//Example: rSigma(5) = 15 (1+2+3+4+5); rSigma(2.5) = 3 (1+2); rSigma(-1) = 0.
+
+
+
+// Recursive Factorial | 
+// Given num, return the product of ints from 1 up to num. If less than zero, treat as zero.
+//If not an integer, truncate. Experts tell us 0! is 1. rFact(3) = 6 (1*2*3). Also, rFact(6.5) = 720 (1*2*3*4*5*6).
+
